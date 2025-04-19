f40f8d1b1fef0950fa780385b3c81be319b6cb50
Dina <dinaalbadarin.gmail.com>
2025-04-19 18:37:04 +0300

    footer & js startup Relates #2

---
+++ guess.js
@@ -0,0 +1,4 @@
+let gameName = "Guess The Word";
+document.title = gameName;
+document.querySelector("h1").innerHTML = gameName;
+document.querySelector("footer").innerHTML = `${gameName} : A Game Created by Dina Albadarin`;
